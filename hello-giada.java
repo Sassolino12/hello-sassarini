f40ac41d96da7edbac08dc3cc98a253850faefcb
Giada Sassarini <giada.sassarini@capellinisauro.edu.it>
2025-04-24 07:31:26 +0000

    Comit iniziale

--- hello-giada.java
+++ hello-giada.java
@@ -1,5 +1,17 @@
 import java.util.io.*;
 public class HelloGiada {
     public static void main(String[] args) {       // entry point from OS           
+        System.out.println("Hello Giada!");
 
-System.out.println("Hello Giada!");
+        // print il mio nome 3 volte
+        for (int i = 0; i < 3; i++) {
+            System.out.println( "Giada!");
+        }
+
+        // print la mia data di nascita
+        System.out.println("4 Ottobre 2007");
+   
+        // print la mia squadra del cuore
+        System.out.println("Forza Napoli");
+    }
+}
