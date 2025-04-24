7f128319115ef6e80842c81789aac5256f658dc6
Giada Sassarini <giada.sassarini@capellinisauro.edu.it>
2025-04-24 06:50:11 +0000

    Commit iniziale

---
+++ HelloGiada.java
@@ -0,0 +1,6 @@
+import java.io.*;
+public class HelloGiada {
+    public static void main(String[] args) {       // entry point from OS       
+    System.out.println("Hello Giada!");
+    }
+}
---
+++ hello-giada.java
@@ -0,0 +1,5 @@
+import java.util.io.*;
+public class HelloGiada {
+    public static void main(String[] args) {       // entry point from OS           
+
+System.out.println("Hello Giada!");
