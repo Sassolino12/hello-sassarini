327acdfb54fe0c1f5fa7e492e3acb5dac7472e7d
Giada Sassarini <giada.sassarini@capellinisauro.edu.it>
2025-05-08 06:59:11 +0000

    Aggiunta stampa da VECTOR

--- hello-giada.java
+++ hello-giada.java
@@ -1,21 +1,19 @@
 import java.util.io.*;
 public class HelloGiada {
     public static void main(String[] args) {       // entry point from OS           
-        System.out.println("Hello Giada!");
-
-        // print il mio nome 3 volte
-        for (int i = 0; i < 3; i++) {
-            System.out.println( "Giada!");
-        } 
-        // print la mia data di nascita
-        System.out.println("4 Ottobre 2007");
-        
-        // print la mia squadra del cuore
-        System.out.println("Forza Napoli");
-    }
-    //Array con il mio nome
+    System.out.println("Hello Giada");
+        //Array con il mio nome
     char[] lettere={'G','i','a','d','a'};
     // print il mio nome lettera per lettera
+    for(char lettera: lettere){
+        System.out.println(lettera);
+    }
+    Vector<Character> lettere=new Vector<Character>();
+    lettere.add('G');
+    lettere.add('i');
+    lettere.add('a');
+    lettere.add('d');
+    lettere.add('a');
     for(char lettera: lettere){
         System.out.println(lettera);
     }
